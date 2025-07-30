b5f98e02d969c2c325d15d2a66ba8a4a7e4d6db8
ShadowShark99 <kadenwchan@gmail.com>
2025-07-30 11:19:40 -0500

    Add links for html get request

--- basic-info/index.js
+++ basic-info/index.js
@@ -1,6 +1,61 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
+
+const app = express();
+
+//express server
+
+let page404;
+let pageIndex;
+let pageAbout;
+let pageContact;
+
+const filePathI = path.join(__dirname, "index.html");
+fs.readFile(filePathI, (err, data) => {
+  //if (!err) app.get("/", (req, res) => res.end(data));
+  if (!err) pageIndex = data;
+});
+
+const filePathA = path.join(__dirname, "about.html");
+fs.readFile(filePathA, (err, data) => {
+  //if (!err) app.get("/about", (req, res) => res.end(data));
+  if (!err) pageAbout = data;
+});
+
+const filePathC = path.join(__dirname, "contact-me.html");
+fs.readFile(filePathC, (err, data) => {
+  //if (!err) app.get("/contact-me", (req, res) => res.end(data));
+  if (!err) pageContact = data;
+});
+
+// 404 handler for all other routes
+const filePath404 = path.join(__dirname, "404.html");
+fs.readFile(filePath404, (err, data) => {
+  //   app.use((req, res) => {
+  //     if (!err) {
+  //       res.status(404).end(data);
+  //     } else {
+  //       res.status(404).send("404 Not Found");
+  //     }
+  //   }
+  // );
+  if (!err) page404 = data;
+});
+
+app.get("/", (req, res) => res.end(pageIndex));
+app.get("/about", (req, res) => res.end(pageAbout));
+app.get("/contact-me", (req, res) => res.end(pageContact));
+app.use((req, res) => res.end(page404));
+
+const PORT2 = process.env.PORT || 3000;
+
+app.listen(PORT2, () => {
+  console.log(`My express app - listning on PORT: ${PORT2}`);
+});
+
+//classic server
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
